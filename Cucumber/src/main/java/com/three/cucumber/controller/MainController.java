d4a1b81e6512e27ea250413ef2bd39fcae6389b2
jk072 <jk072@DESKTOP-R23DNOJ>
2024-01-24 15:19:58 +0900

    update

--- Cucumber/src/main/java/com/three/cucumber/controller/MainController.java
+++ Cucumber/src/main/java/com/three/cucumber/controller/MainController.java
@@ -68,7 +68,7 @@ public class MainController {
 	}
 
 	// 상세 페이지로 이동
-	// 쿠키 세팅하고 거래글 상세내용으로 들어가면 쿠키 없음 조회수 +1 증가
+	// 쿠키 세팅하고 거래글 상세내용으로 들어가면 쿠키 없을 때 조회수 +1 증가
 	@RequestMapping("detail.do")
 	public String detail(@CookieValue(value = "view") String cookie, ReportQnaVO rqvo,HttpServletRequest request, HttpServletResponse response,
 			 Integer postId, Model model ) throws Exception {  
